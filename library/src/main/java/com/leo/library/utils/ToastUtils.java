6b17dcd99468e5c1f374d9203a3aab2e1da0d817
HoyangLau <782094839@qq.com>
2018-09-07 18:39:20 +0800

    add

--- library/src/main/java/com/leo/library/utils/ToastUtils.java
+++ library/src/main/java/com/leo/library/utils/ToastUtils.java
@@ -1,8 +1,29 @@
 package com.leo.library.utils;
 
+import android.content.Context;
+import android.support.annotation.StringRes;
+import android.widget.Toast;
+
 /**
  * Creator: Leoying
  * Date: 2018/9/7 15:36
  */
 public class ToastUtils {
+    private static Toast toast;
+
+
+    public static void showToast(Context context, @StringRes int id) {
+        showToast(context, context.getText(id));
+    }
+
+    public static void showToast(Context context, CharSequence s) {
+        if (toast == null) {
+            toast = new Toast(context);
+            toast.setDuration(Toast.LENGTH_SHORT);
+        }
+        toast.setText(s);
+        toast.show();
+    }
+
+
 }
